fa86aaae2e0732b2239bfc577c4ed51600c1fdfc
Neil John Medalla <medallaneiljohn@gmail.com>
2021-05-02 20:26:32 -0600

    first commit

---
+++ pages/profile/image.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Image = () => {
+  return (
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Neil"
+    />
+  );
+};
+
+export default Image;
